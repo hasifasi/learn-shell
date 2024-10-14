f186ce62bc8652c77a50cd46df17559c073fbd44
Mohammed <i23281@verisk.com>
2024-10-14 00:16:19 -0400

    Colors

--- 01-print.sh
+++ 01-print.sh
@@ -1,3 +1,23 @@
 
-
+# echo or printf command is used to print
 echo "I love U"
+printf " Print I love U"
+
+#print colors
+#sysntax: echo -e "\e[COLmHello World\e[0m"
+# echo -e, -e - enable colors
+# \e[COLm - Tell which color
+# \e[0m - Disable color
+
+# COL       - CODE
+# RED       - 31
+# GREEN     - 32
+# YELLOW    - 33
+# BLUE      - 34
+# MAGENTA   - 35
+# CYAN      - 36
+
+echo -e "\e[35mHello World\[e0m"
+echo -e "\e[33m  Hello space World  \e[0m"
+
+
